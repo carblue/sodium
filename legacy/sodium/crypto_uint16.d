4547262cedce286b4f0cd605e978fa205e3c99be
carblue <ka6613-496@online.de>
2017-03-15 13:38:18 +0100

    remove legacy subPackage

--- legacy/sodium/crypto_uint16.d
+++
@@ -1,9 +0,0 @@
-/*
-Written in the D programming language.
-For git maintenance (ensure at least one congruent line with originating C header):
-#define crypto_uint16_H
-*/
-
-module sodium.crypto_uint16;
-
-alias crypto_uint16 = ushort;
--- legacy/sodium/crypto_uint8.d
+++
@@ -1,9 +0,0 @@
-/*
-Written in the D programming language.
-For git maintenance (ensure at least one congruent line with originating C header):
-#define crypto_uint8_H
-*/
-
-module sodium.crypto_uint8;
-
-alias crypto_uint8 = ubyte;
