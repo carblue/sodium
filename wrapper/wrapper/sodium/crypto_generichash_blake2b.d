bb45a18652ea7b8b328ba6df0dbdba021250715e
carblue <bluecars@posteo.eu>
2019-06-01 23:37:43 +0200

    update (9) to C-source version 1.0.18

--- wrapper/wrapper/sodium/crypto_generichash_blake2b.d
+++ wrapper/wrapper/sodium/crypto_generichash_blake2b.d
@@ -14,13 +14,8 @@ unittest
 //  import std.algorithm.comparison : equal;
   import std.stdio : writeln;
   debug writeln("unittest block 1 from sodium.crypto_generichash_blake2b.d");
-version (D_LP64) {
-  static assert(crypto_generichash_blake2b_state.last_node.offsetof == 352+8);
-//  writeln("crypto_generichash_blake2b_state.sizeof:  ", crypto_generichash_blake2b_state.sizeof);  // 384
-//  writeln("crypto_generichash_blake2b_state.alignof: ", crypto_generichash_blake2b_state.alignof); // 8
-}
-else
-  static assert(crypto_generichash_blake2b_state.last_node.offsetof == 352+4);
+  debug writeln("crypto_generichash_blake2b_state.sizeof:  ", crypto_generichash_blake2b_state.sizeof);  // 384
+  debug writeln("crypto_generichash_blake2b_state.alignof: ", crypto_generichash_blake2b_state.alignof); // 64
 }
 
 /+
